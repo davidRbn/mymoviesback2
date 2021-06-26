30aa77f84b97aa0e797abc7788611d1e077e2802
ruben <davidruben14@gmail.com>
2021-06-26 14:27:41 +0200

    test deployment

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use('/api/',apiRouter)
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"),
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"),
+    next(),
+  }),
 
 app.get('/',(req,res) =>{
     res.setHeader('Content-Type','text/html')
