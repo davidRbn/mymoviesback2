abd0d72139aaed9219d95561779bf5fbe3a52c87
ruben <davidruben14@gmail.com>
2024-02-08 20:38:06 +0100

    correction bug colonne idFavoris

--- models/favori.js
+++ models/favori.js
@@ -1,31 +1,30 @@
-'use-strict'
+"use-strict";
 
-const models = require('../models')
-
+const models = require("../models");
 
 module.exports = (sequelize, DataTypes) => {
+  const Favori = sequelize.define("Favoris", {
+    idFavoris: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userUid: DataTypes.STRING,
+    idMovie: DataTypes.STRING,
+    typeMovie: DataTypes.STRING,
+    img: DataTypes.STRING,
+    title: DataTypes.STRING,
+    details: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    updatedAt: DataTypes.DATE,
+    createdAt: DataTypes.DATE,
+  });
 
-    const Favori = sequelize.define('Favoris',{    
-        idFavoris: {type : DataTypes.STRING, primaryKey: true,autoIncrement: true },
-        userUid: DataTypes.STRING,
-        idMovie : DataTypes.STRING , 
-        typeMovie : DataTypes.STRING,
-        img : DataTypes.STRING,
-        title : DataTypes.STRING,
-        details: DataTypes.STRING,
-        description : DataTypes.TEXT,
-        updatedAt : DataTypes.DATE,
-        createdAt : DataTypes.DATE
+  //model favori
 
-    })
+  // Favori.associate = models => {
+  //     Favori.belongsTo(models.Users)
+  // }
 
- //model favori
-    
-    // Favori.associate = models => {
-    //     Favori.belongsTo(models.Users)
-    // }
-
-  return Favori
-}
-
-
+  return Favori;
+};
